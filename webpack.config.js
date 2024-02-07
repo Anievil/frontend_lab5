ef8e5f568ef0d21c7e536f09e5a3010789925265
Your Name <you@example.com>
2024-02-07 16:56:52 +0200

    'lab7'

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,61 @@
-const path = require('path')
-const glob = require('glob');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const glob = require("glob");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const webpackConfig = {
-	mode: 'production',
-	entry: {
-		main: path.resolve(__dirname, './src/index.js')
-	},
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
 
-	output: {
-		filename: 'index.bundle.js',
-		path: path.resolve(__dirname, './dist')
-	},
-	performance: {
-		maxAssetSize: 2000000,
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
+  output: {
+    filename: "index.bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  performance: {
+    maxAssetSize: 2000000,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
 
-		new MiniCssExtractPlugin({
-			filename: 'styles.css',
-		}),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
 
-		new CopyPlugin({
-			patterns: [
-				{
-					from: './src/assets/images',
-					to: 'assets/images'
-				}
-			]
-		}),
-		new HtmlWebpackPlugin({
-			title: 'webpack Boilerplate',
-			template: path.resolve(__dirname, './src/template.html'), // шаблон
-			filename: 'template.html', // ім'я вихідного файлу
-		}),
-		...glob.sync('./src/pages/**/*.html').map((filePath) => {
-			const fileName = path.basename(filePath);
-			return new HtmlWebpackPlugin({
-				filename: fileName,
-				template: filePath,
-				inject: true,
-				chunks: ['main'],
-			});
-		}),
-	],
-	module: {
-		// rules: [
+    new CopyPlugin(
+      {
+        patterns: [
+          {
+            from: "./src/assets/images",
+            to: "assets/images",
+          },
+        ],
+      },
+      {
+        from: "./src/scss/print.css", // Путь к исходному файлу, который нужно скопировать
+        to: "print.css", // Путь к месту, куда нужно скопировать файл
+      }
+    ),
+    new HtmlWebpackPlugin({
+      title: "webpack Boilerplate",
+      template: path.resolve(__dirname, "./src/template.html"), // шаблон
+      filename: "template.html", // ім'я вихідного файлу
+    }),
+    ...glob.sync("./src/pages/**/*.html").map((filePath) => {
+      const fileName = path.basename(filePath);
+      return new HtmlWebpackPlugin({
+        filename: fileName,
+        template: filePath,
+        inject: true,
+        chunks: ["main"],
+      });
+    }),
+  ],
+  module: {
+    // rules: [
     //   {
     //     test: /\.scss$/,
     //     use: [
@@ -59,24 +65,17 @@ const webpackConfig = {
     //     ],
     //   },
     // ],
-		rules: [
-			{
-				test: /\.scss$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
-					'sass-loader',
-				],
-			},
-			{
-				test: /\.css$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
-				],
-			},
-		],
-	},
-}
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+};
 
-module.exports = webpackConfig
+module.exports = webpackConfig;
