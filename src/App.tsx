ade4cd8f8b51e88210dab18a61f1809f371ce2f6
Your Name <you@example.com>
2024-02-07 17:03:03 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ExpenseList from './components/ExpenseList';
+
+function App() {
+	return (
+		<div>
+			<h2>My Expenses tempalte</h2>
+			<ExpenseList />
+		</div>
+	);
+}
+
+export default App;
